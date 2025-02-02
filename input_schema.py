3346ba1fab84e94c6c2881d120f949eacd338564
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2025-02-03 01:21:42 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,6 +4,30 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["[{\"role\":\"developer\",\"content\":\"You are a helpful assistant.\"},{\"role\":\"user\",\"content\":\"Hello!\"}]"]
+    },
+    "temperature": {
+        'datatype': 'FP32',
+        'required': False,
+        'shape': [1],
+        'example': [0.7]
+    },
+    "max_completion_tokens": {
+        'datatype': 'INT32',
+        'required': False,
+        'shape': [1],
+        'example': [250]
+    },
+    "presence_penalty": {
+        'datatype': 'FP32',
+        'required': False,
+        'shape': [1],
+        'example': [0.5]
+    },
+    "seed": {
+        'datatype': 'INT32',
+        'required': False,
+        'shape': [1],
+        'example': [1234]
     }
 }
 OPENAI_CLIENT_COMPATIBLITY = True
