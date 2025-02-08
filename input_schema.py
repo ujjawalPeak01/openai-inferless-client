d0df42f977fab387f81ea8f3d9ab06c97df63276
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2025-02-08 17:42:00 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -9,25 +9,25 @@ INPUT_SCHEMA = {
         'datatype': 'FP32',
         'required': False,
         'shape': [1],
-        'example': [0.7]
+        'example': [0.5]
     },
     "max_length": {
         'datatype': 'INT32',
         'required': False,
         'shape': [1],
-        'example': [250]
+        'example': [256]
     },
     "repetition_penalty": {
         'datatype': 'FP32',
         'required': False,
         'shape': [1],
-        'example': [0.5]
+        'example': [1.2]
     },
     "top_p": {
         'datatype': 'FP32',
         'required': False,
         'shape': [1],
-        'example': [0.9]
+        'example': [0.7]
     }
 }
 OPENAI_CLIENT_COMPATIBLITY = True
